af955b4126af861533b968387ed62f7609e92900
Dana Schultz <dana.schultz@outlook.com>
2020-10-05 19:10:49 -0500

    Add Platform model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_000427) do
+ActiveRecord::Schema.define(version: 2020_10_06_000818) do
 
   create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "platforms", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
