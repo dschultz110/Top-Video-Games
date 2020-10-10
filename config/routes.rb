7858123cb9765826f98b72f20121bf43b2b922aa
Dana Schultz <dana.schultz@outlook.com>
2020-10-10 15:53:06 -0500

    Show information on index and show pages for games

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :games, only: %i[index show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "games#index"
 end
