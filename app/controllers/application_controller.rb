d11b297a89153f6b65f33519a383b3d58616158d
Dana Schultz <dana.schultz@outlook.com>
2020-10-10 19:22:51 -0500

    Add sidebar and more styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :find_info
+
+  def find_info
+    @sidebar_genres = Genre.order("name ASC")
+    @sidebar_platforms = Platform.order("name ASC")
+    @sidebar_games = Game.order("sales DESC").limit(20)
+  end
 end
