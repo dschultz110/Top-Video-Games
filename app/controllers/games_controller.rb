7938961252154c73183a9598cfc58aec3a015e53
Dana Schultz <dana.schultz@outlook.com>
2020-10-14 14:21:37 -0500

    Add pagination on games index page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,9 +3,10 @@ class GamesController < ApplicationController
     if params[:search]
       @search_term = params[:search]
       @search = true
-      @games = Game.search_by(@search_term)
+      @games = Game.search_by(@search_term).paginate(:page => params[:page], per_page: 50)
     else
-      @games = Game.includes(:publisher, :genre).order("name ASC")
+      @games = Game.includes(:publisher, :genre)
+                    .order("name ASC").paginate(:page => params[:page], per_page: 50)
     end
   end
 
